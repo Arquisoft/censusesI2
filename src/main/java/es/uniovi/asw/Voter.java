6108a9e6d69731c5b5bd7a3517315af9b0b6a4a1
alexbrks <alex_brks@hotmail.com>
2016-02-13 14:08:26 +0100

    class Voter, class PasswordGenerator, modifications in ReadXlsx
    implemented the object container voter and the password generator

---
+++ src/main/java/es/uniovi/asw/Voter.java
@@ -0,0 +1,16 @@
+package es.uniovi.asw;
+
+public class Voter {
+
+	private String name, email, nif, pollStCode, password;
+	
+	public Voter(String name, String email, String nif, String pollStCode, String password) {
+		this.name = name;
+		this.email = email;
+		this.nif = nif;
+		this.pollStCode = pollStCode;
+		this.password = password;
+	}
+	
+	
+}
