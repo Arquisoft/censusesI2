9583fa38984fb61eab1ab3c1999339a72aecee69
S-Casero <kxerosc4@gmail.com>
2016-02-22 02:46:20 +0100

    Update CliTest.java

--- src/test/java/es/uniovi/asw/CliTest.java
+++ src/test/java/es/uniovi/asw/CliTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class CliTest {
 	
-	String[] s = {"-test", "This is a test."};
+	String[] s = {"-test", "This is a test.","-ic", "-od"};
 	String testText;
 	@Test
 	public void test() {
